15cb3efe16fe2471f3b13110516912582976a0af
xb <1289980732@qq.com>
2020-02-11 14:08:29 +0800

    "环境搭建"

---
+++ src/test/java/com/tydic/signaltest/SignaltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tydic.signaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignaltestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
